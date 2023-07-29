69aa57e1694d224be6451039630b7a8d082b45c0
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-07-29 18:23:42 +0200

    moving the connection DB to a separate file

---
+++ db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const connectToDB = () => {
+  const NODE_ENV = process.env.NODE_ENV;
+  let dbUri = "";
+
+  if (NODE_ENV === "production")
+    dbUri = `mongodb+srv://lukasz:${process.env.DB_PASS}@cluster0.sjmpwid.mongodb.net/AdsAppDB?retryWrites=true&w=majority`;
+  else dbUri = "mongodb://localhost:27017/AdsAppDB";
+
+  mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+  const db = mongoose.connection;
+
+  db.once("open", () => {
+    console.log("Connected to the database");
+  });
+
+  db.on("error", (err) => console.log("Error " + err));
+};
+
+module.exports = connectToDB;
