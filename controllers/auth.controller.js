f68a1ab4f89590b24dfc773e36efd82a0e4c0bdd
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-07-29 18:18:24 +0200

    add login controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcryptjs");
+
 exports.register = async (req, res) => {
   try {
     const { login, password, avatar, phoneNumber } = req.body;
@@ -36,4 +37,27 @@ exports.register = async (req, res) => {
   }
 };
 
-exports.login = async (req, res) => {};
+exports.login = async (req, res) => {
+  try {
+    const { login, password } = req.body;
+    if (
+      login &&
+      typeof login === "string" &&
+      password &&
+      typeof password === "string"
+    ) {
+      const user = await User.findOne({ login });
+      if (!user) {
+        res.status(400).json({ message: "Login or password are incorrect" });
+      } else {
+        if (bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: "Login successful" });
+        } else {
+          res.status(400).json({ message: "Login or password are incorrect" });
+        }
+      }
+    } else {
+      res.status(400).json({ message: "Bad request" });
+    }
+  } catch (err) {}
+};
