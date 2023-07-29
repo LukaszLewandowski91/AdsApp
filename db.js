e460c9c94cc2737b58d6e3419bbe7b00974d54f4
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-07-29 18:32:05 +0200

    modify connection

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const connectToDB = () => {
   let dbUri = "";
 
   if (NODE_ENV === "production")
-    dbUri = `mongodb+srv://lukasz:${process.env.DB_PASS}@cluster0.sjmpwid.mongodb.net/AdsAppDB?retryWrites=true&w=majority`;
+    dbUri = `mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASS}@cluster0.sjmpwid.mongodb.net/AdsAppDB?retryWrites=true&w=majority`;
   else dbUri = "mongodb://localhost:27017/AdsAppDB";
 
   mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
