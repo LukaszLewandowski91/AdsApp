8654c9638bd52a2c3dbf6d1da669c95ab9ce0e5b
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-07-29 17:35:07 +0200

    modify route getUser

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const UserController = require("../controllers/users.controller");
 
 router.post("/register", UserController.register);
-router.get("/user", UserController.getUser);
+router.get("/user/:id", UserController.getUser);
 
 module.exports = router;
