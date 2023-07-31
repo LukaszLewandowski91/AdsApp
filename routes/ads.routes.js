315f2e6ff454eccf09922aa6b540c90b6204e485
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-07-31 10:31:00 +0200

    add authMiddleware to routes

--- routes/ads.routes.js
+++ routes/ads.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../utils/authMiddleware");
 const AdController = require("../controllers/ads.controller");
 
 router.get("/ads", AdController.getAllAds);
 router.get("/ads/:id", AdController.getAdById);
 router.get("/ads/search/:searchPhrase", AdController.getBySearchPhrase);
-router.post("/ads", AdController.addNewAd);
-router.delete("/ads/:id", AdController.deleteAd);
-router.put("/ads/:id", AdController.updateAd);
+router.post("/ads", authMiddleware, AdController.addNewAd);
+router.delete("/ads/:id", authMiddleware, AdController.deleteAd);
+router.put("/ads/:id", authMiddleware, AdController.updateAd);
 
 module.exports = router;
