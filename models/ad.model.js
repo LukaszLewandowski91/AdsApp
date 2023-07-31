e02082304205dc0930161eaa0886151479d949f5
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-07-31 18:26:39 +0200

    change type for dateand price

--- models/ad.model.js
+++ models/ad.model.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const adSchema = new mongoose.Schema({
   title: { type: String, required: true, minLength: 10, maxLength: 50 },
   description: { type: String, required: true, minLength: 20, maxLength: 1000 },
-  publishDate: { type: Date, required: true },
+  publishDate: { type: String, required: true },
   image: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: String, required: true },
   location: { type: String, required: true },
   userId: { type: String, required: true, ref: "User" },
 });
