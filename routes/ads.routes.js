37dc541ba326926b5cbce9c6663d6cbe7cd628a9
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-07-31 18:27:48 +0200

    add multer to ads routes

--- routes/ads.routes.js
+++ routes/ads.routes.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../utils/authMiddleware");
 const AdController = require("../controllers/ads.controller");
+const imageUpload = require("../utils/imageUpload");
 
 router.get("/ads", AdController.getAllAds);
 router.get("/ads/:id", AdController.getAdById);
 router.get("/ads/search/:searchPhrase", AdController.getBySearchPhrase);
-router.post("/ads", authMiddleware, AdController.addNewAd);
+router.post(
+  "/ads",
+  authMiddleware,
+  imageUpload.single("image"),
+  AdController.addNewAd
+);
 router.delete("/ads/:id", authMiddleware, AdController.deleteAd);
 router.put("/ads/:id", authMiddleware, AdController.updateAd);
 
