0dd74032c11f005d32693152e576d32e478eb534
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-08-01 13:30:09 +0200

    add Provider ad Router to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/global.scss";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { Router } from "express";
 
 const Root = () => {
-  <App />;
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>;
 };
 
 ReactDOM.render(<Root />, document.getElementById("root"));
