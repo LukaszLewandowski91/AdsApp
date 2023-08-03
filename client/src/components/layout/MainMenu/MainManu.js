3f4bed98038e3269926b9840003bd03cb8b663d5
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-08-03 09:47:55 +0200

    update MainMenu

--- client/src/components/layout/MainMenu/MainManu.js
+++ client/src/components/layout/MainMenu/MainManu.js
@@ -1,22 +1,33 @@
-import { useState } from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const MainMenu = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Navbar.Brand href="/">Ads App</Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className="justify-content-end">
         <Nav>
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/login">Sign In</Nav.Link>
-          <Nav.Link href="/logout">Sign Out</Nav.Link>
-          <Nav.Link href="/register">Sign Up</Nav.Link>
+          <Nav.Link>
+            <Link to="/" className="text-white-50">
+              Home
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/login" className="text-white-50">
+              Sign In
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/logout" className="text-white-50">
+              Sign Out
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/register" className="text-white-50">
+              Sign Up
+            </Link>
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
