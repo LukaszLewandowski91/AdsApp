5e7911d62a5a66605d4d0bbd118e8164672966ba
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-08-03 10:07:55 +0200

    add logout route

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import MainLayout from "./components/layout/MainLayout/MainLayout";
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/pages/Register/Register";
 import Login from "./components/pages/Login/Login";
+import Logout from "./components/pages/Logout/Logout";
 const App = () => (
   <MainLayout>
     <Routes>
       <Route path="/" element={<AdsBoard />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/logout" element={<Logout />} />
     </Routes>
   </MainLayout>
 );
