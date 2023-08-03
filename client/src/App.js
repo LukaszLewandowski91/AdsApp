587b9e48051d5735b6f937376d449cddede7ae00
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-08-03 09:45:11 +0200

    add login route

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import AdsBoard from "./components/features/AdsBoard/AdsBoard";
 import MainLayout from "./components/layout/MainLayout/MainLayout";
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/pages/Register/Register";
+import Login from "./components/pages/Login/Login";
 const App = () => (
   <MainLayout>
     <Routes>
       <Route path="/" element={<AdsBoard />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </MainLayout>
 );
