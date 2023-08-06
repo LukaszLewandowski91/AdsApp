6b5b95c9642501d11f540a64074e4f1185f86cdf
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-08-06 15:10:36 +0200

    add link to create new ad

--- client/src/components/features/AdsBoard/AdsBoard.js
+++ client/src/components/features/AdsBoard/AdsBoard.js
@@ -1,12 +1,15 @@
 import { useDispatch, useSelector } from "react-redux";
 import { getAds, loadAdsRequest } from "../../../redux/adsRedux";
 import { useEffect } from "react";
-import { Spinner, Container, Row } from "react-bootstrap";
+import { Spinner, Container, Row, Button } from "react-bootstrap";
 import AdCard from "../AdCard/AdCard";
+import { Link } from "react-router-dom";
+import { getUser } from "../../../redux/usersRedux";
 
 const AdsBoard = () => {
   const dispatch = useDispatch();
   const ads = useSelector(getAds);
+  const user = useSelector(getUser);
 
   useEffect(() => {
     dispatch(loadAdsRequest());
@@ -15,6 +18,11 @@ const AdsBoard = () => {
   return (
     <Container>
       <Row>
+        {user && (
+          <Link to="/add" className="mb-4">
+            <Button variant="primary">Add advertisement</Button>
+          </Link>
+        )}
         {!ads && (
           <Spinner
             animation="border"
