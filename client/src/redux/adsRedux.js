b1773e774909ad435a8d4fe5c196ccce3bb14f3b
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-08-06 14:39:03 +0200

    add edit request

--- client/src/redux/adsRedux.js
+++ client/src/redux/adsRedux.js
@@ -10,10 +10,12 @@ const createActionName = (name) => `app/${reducerName}/${name}`;
 const LOAD_ADS = createActionName("LOAD_ADS");
 const ADD_AD = createActionName("ADD_AD");
 const REMOVE_AD = createActionName("REMOVE_AD");
+const EDIT_AD = createActionName("EDIT_AD");
 
 export const loadAds = (payload) => ({ type: LOAD_ADS, payload });
 export const addAd = (payload) => ({ type: ADD_AD, payload });
 export const removeAd = (payload) => ({ type: REMOVE_AD, payload });
+export const editAd = (payload) => ({ type: EDIT_AD, payload });
 /* THUNKS */
 
 export const loadAdsRequest = () => {
@@ -30,9 +32,36 @@ export const loadAdsRequest = () => {
 export const addAdRequest = (ad) => {
   return async (dispatch) => {
     try {
-      let res = await axios.post(`${API_URL}api/ads`, ad);
+      let res = await axios.post(`${API_URL}api/ads`, ad, {
+        withCredentials: true,
+      });
       dispatch(addAd(res));
       console.log("res", res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+};
+
+export const editAdRequest = (ad, id) => {
+  return async (dispatch) => {
+    try {
+      await axios.put(`${API_URL}api/ads/${id}`, ad, {
+        withCredentials: true,
+      });
+      dispatch(editAd(ad));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+};
+
+export const removeAdRequest = (id) => {
+  return async (dispatch) => {
+    try {
+      await axios
+        .delete(`${API_URL}api/ads/${id}`, { withCredentials: true })
+        .then(() => dispatch(removeAd(id)));
     } catch (e) {
       console.log(e);
     }
@@ -49,6 +78,10 @@ const adsReducer = (statePart = [], action) => {
       return [...action.payload];
     case REMOVE_AD:
       return [...statePart.filter((ad) => ad._id !== action.payload)];
+    case EDIT_AD:
+      return statePart.map((ad) =>
+        ad._id === action.payload._id ? { ...ad, ...action.payload } : ad
+      );
     default:
       return statePart;
   }
